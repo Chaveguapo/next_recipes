0e3f23922ba319d10068a10b7b23f321725c5b6c
Chaveguapo <catakeas@gmail.com>
2024-10-03 13:21:06 +0200

    Creation of components

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,29 @@
-import { Button } from "@/components/ui/button";
-
+import { Badge } from "@/components/ui/badge";
 export default function Home() {
+  const cuisines: Array<string> = [
+    "All",
+    "Japanese",
+    "Mexican",
+    "Pakistani",
+    "Indian",
+    "American",
+    "Mediterranean",
+    "Italian",
+    "Greek",
+  ];
   return (
     <div>
-      <Button>Click me</Button>
+      {cuisines.map((cuisine, idx) => (
+        <Badge
+          key={`${cuisine}-${idx}`}
+          variant={"outline"}
+          className="border-orange-800 text-gray-900
+         text-lg mx-2 my-1 hover:cursor-pointer bg-orange-50 hover:scale-110 ease-in duration-200 
+         "
+        >
+          {cuisine}
+        </Badge>
+      ))}
     </div>
   );
 }
